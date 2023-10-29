30efe3b29511dce959371e37957af4ea57b615b4
tejasvijb <jbtejasvi@gmail.com>
2023-10-29 18:33:52 +0530

    added api's

--- quote-api-starting/server.js
+++ quote-api-starting/server.js
@@ -8,3 +8,35 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
+
+app.get('/api/quotes/random', (req, res, next) => {
+    res.send({
+        quote: getRandomElement(quotes)
+    });
+
+})
+
+app.get('/api/quotes', (req, res, next) => {
+    if (req.query.person) {
+        const quotesByPerson = quotes.filter(quote => quote.person === req.query.person);
+        res.send({ quotes: quotesByPerson });
+    } else {
+        res.send({ quotes: quotes });
+    }
+})
+
+app.post('/api/quotes', (req, res, next) => {
+    const newQuote = {
+        quote: req.query.quote,
+        person: req.query.person
+    };
+    if (newQuote.quote && newQuote.person) {
+        quotes.push(newQuote);
+        res.send({ quote: newQuote });
+    } else {
+        res.status(400).send();
+    }
+})
